d089019ce93def68e762d0e8ea8c1045667c272a
martinezGomez <emartinezgomez1985@gmail.com>
2022-02-28 11:15:12 +0100

    fin seccion 3

---
+++ src/app/shared/breadcumbs/breadcumbs.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcumbs',
+  templateUrl: './breadcumbs.component.html',
+  styles: [
+  ]
+})
+export class BreadcumbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
